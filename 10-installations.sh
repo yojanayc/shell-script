2d86d07da15238ce6441038aa55a7fe0f5b932ca
yojanayc <yojanasajja01@gmail.com>
2024-08-26 15:35:26 -0500

    shell-script

--- 10-installations.sh
+++ 10-installations.sh
@@ -8,3 +8,25 @@ then
 else
     echo "You are a super user"
 fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installation of mysql...FAILURE"
+    exit 1
+else
+    echo "Installation of mysql...SUCCESS"
+fi
+
+dnf install git -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installation of git...FAILURE"
+    exit 1
+else
+    echo "Installation of Git...SUCCESS"
+fi
+
+echo "is script proceeding?"
