8b3d98fe8a4f86917c472759f3a3c6ef98f5f94d
yojanayc <yojanasajja01@gmail.com>
2024-08-26 14:00:07 -0500

    shell-script

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Please enter username::"
+
+read -s USERNAME #here USERNAME is variable
+
+echo "Please enter password::"
+
+read -s PASSWORD
+
+echo "Username is: $USERNAME, Password is: $PASSWORD"
