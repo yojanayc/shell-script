8f4da93f3c1e6b9b02aa14266a6972914250ad46
yojanayc <yojanasajja01@gmail.com>
2024-08-26 13:47:07 -0500

    shell-script

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hello Suresh, How are you?"
+echo "Suresh:: Hi Ramesh, I am fine. How are you?"
+echo "Ramesh: I am fine too. how is your work?"
+echo "Suresh:: not bad. I am thinking to upgrade to DevOps"
---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#declaring of varible
+PERSON1=$1
+PERSON2=$2
+
+#referring variable
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: I am fine too. how is your work?"
+echo "$PERSON2:: not bad. I am thinking to upgrade to DevOps"
