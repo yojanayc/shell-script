9c4ee8aa2f44d14df76d3bc4bff8c7bc17822917
yojanayc <yojanasajja01@gmail.com>
2024-08-29 14:18:37 -0500

    shell-script

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -31,7 +31,7 @@ for i in $@
 do
     echo  "packages to install: $i"
     dnf list installed $i &>>$LOGFILE
-    if [ $? -eq 0]
+    if [ $? -eq 0 ]
     then 
         echo "$i already installed...SKIPPING"
     else
