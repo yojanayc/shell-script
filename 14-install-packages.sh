3a835763322e70a33c0c5003c47858643aa18b20
yojanayc <yojanasajja01@gmail.com>
2024-08-29 14:23:36 -0500

    shell-script

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-USER=$(id -u)
+USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
@@ -19,22 +19,23 @@ VALIDATE(){
     fi
 }
 
-if [ $USER -ne 0 ]
-then 
-    echo "Please run the script with root access"
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
 else
-    echo "You are a super user"
+    echo "You are super user."
 fi
-
 
 for i in $@
 do
-    echo  "packages to install: $i"
+    echo "package to install: $i"
     dnf list installed $i &>>$LOGFILE
     if [ $? -eq 0 ]
-    then 
-        echo "$i already installed...SKIPPING"
+    then
+        echo -e "$i already installed...$Y SKIPPING $N"
     else
-        echo "$i not installed ....Need to install"
+        dnf install $i -y &>>$LOGFILE
+        VALIDATE $? "Installation of $i"
     fi
-done 
+done
