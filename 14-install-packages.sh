d4c22b1013ecedd4ca95d288ef22104706dbd867
yojanayc <yojanasajja01@gmail.com>
2024-08-29 14:17:33 -0500

    shell-script

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -4,6 +4,20 @@ USER=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
+}
 
 if [ $USER -ne 0 ]
 then 
