0f407b897b7d82b926dc1d020bd7cf21bbf415bc
yojanayc <yojanasajja01@gmail.com>
2024-09-11 14:01:48 -0500

    expense-shell

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -5,7 +5,7 @@ COURSE="DevOps from Current Script"
 echo "Before calling other script, course: $COURSE"
 echo "Process ID of current shell script: $$"
 
-./16-other-script.sh
+./16-otherscript.sh
 
 #source ./16-otherscript.sh
 
