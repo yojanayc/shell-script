1b5efb55626c3a98dd994d2359946de43b1c4996
yojanayc <yojanasajja01@gmail.com>
2024-09-12 13:34:01 -0500

    expense-shell

--- sample.sh
+++ sample.sh
@@ -7,10 +7,10 @@ YELLOW='\033[1;33m'
 NC='\033[0m'  # No Color (Reset)
 
 # Success message
-echo -e "${GREEN}Success: Operation completed successfully!${NC}"
+echo -e "${GREEN}Success: It's completed successfully!${NC}"
 
 # Warning message
-echo -e "${YELLOW}Warning: This is a warning message.${NC}"
+echo -e "${YELLOW}Warning: There is a warning message.${NC}"
 
 # Error message
-echo -e "${RED}Error: Something went wrong!${NC}"
+echo -e "${RED}Error: Oops something went wrong!${NC}"
