fce5d7fef9edb76ba1ce78af5564d4f18d45a21c
yojanayc <yojanasajja01@gmail.com>
2024-09-13 12:12:11 -0500

    expense-shell

--- sample.sh
+++ sample.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# Redirect stdout (1) and stderr (2) to logfile.log
-exec > /home/ec2-user/logfile.log 2>&1
+LOGFILE="/home/ec2-user/logfile.log"
 
-echo "Starting script..."
-ls /nonexistent_directory  # This will log an error
-echo "Script finished."
+log() {
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" >> "$LOGFILE"
+}
+
+log "Script started"
+# Some operations
+log "Operation completed"
+log "Script finished"
